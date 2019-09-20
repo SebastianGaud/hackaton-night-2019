550f9dec1d0a99b02d6e59f907996491507e0081
SebastianGaud <sebastiano.gaudeano@live.com>
2019-09-21 01:09:59 +0200

    Lite db context

---
+++ hackaton-night-2019/Services/DatabaseContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace hackaton_night_2019.Services
+{
+    public interface IDatabaseContext
+    {
+        LiteCollection<T> Get<T>(string tablename);
+        void Set<T>(T entry,string tablename);
+    }
+
+    [SuppressMessage("ReSharper", "ArrangeThisQualifier")]
+    public class DatabaseContext : IDisposable, IDatabaseContext
+    {
+        private LiteDatabase _db;
+
+        public DatabaseContext()
+        {
+            _db = new LiteDatabase("MyData.db");
+        }
+
+
+        public LiteCollection<T> Get<T>(string tablename)
+        {
+            return _db.GetCollection<T>(tablename);
+        }
+
+        public void Set<T>(T entry,string tablename)
+        {
+            this.Get<T>(tablename).Insert(entry);
+        }
+
+        #region IDisposable
+
+        public void Dispose()
+        {
+            _db?.Dispose();
+        }
+
+        #endregion
+
+    }
+}
