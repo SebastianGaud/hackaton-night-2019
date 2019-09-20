3b185482e7cca0a26419319b60022272e3e9e352
raffaele <raffaele.balzanialice.it>
2019-09-20 20:32:19 +0200

    aggiunto controller dialog

---
+++ hackaton-night-2019/Controllers/DialogController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hackaton_night_2019.Controllers
+{
+    public class DialogController : Controller
+    {
+        public string GetResponseFromDialogFlow(string question)
+        {
+            return null;
+        }
+    }
+}
