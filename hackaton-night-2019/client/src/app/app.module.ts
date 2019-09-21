9e54845f65a0ec0ddc0674fced23bd3f18c080a6
SebastianGaud <sebastiano.gaudeano@live.com>
2019-09-21 08:19:49 +0200

    update

--- hackaton-night-2019/client/src/app/app.module.ts
+++ hackaton-night-2019/client/src/app/app.module.ts
@@ -1,27 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { OpenedchartComponent } from './openedchart/openedchart.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+    NbThemeModule,
+    NbLayoutModule,
+    NbChatModule,
+    NbSpinnerModule
+} from "@nebular/theme";
+import { NbEvaIconsModule } from "@nebular/eva-icons";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    OpenedchartComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'default' }),
-    NbLayoutModule,
-    NbEvaIconsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        NbThemeModule.forRoot({ name: "default" }),
+        NbLayoutModule,
+        NbEvaIconsModule,
+        NbChatModule,
+        NbSpinnerModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- hackaton-night-2019/client/src/app/openedchart/openedchart.component.ts
+++ hackaton-night-2019/client/src/app/openedchart/openedchart.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import { ChartOptions, ChartType, ChartDataSets } from "chart.js";
+import { Label } from "ng2-charts";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-openedchart',
-  templateUrl: './openedchart.component.html',
-  styleUrls: ['./openedchart.component.scss']
+    selector: "app-openedchart",
+    templateUrl: "./openedchart.component.html",
+    styleUrls: ["./openedchart.component.scss"]
 })
 export class OpenedchartComponent implements OnInit {
+    constructor(
+        @Inject("BASE_URL") private baseUrl: string,
+        private http: HttpClient
+    ) {
+        this.http
+            .get<any>(`${baseUrl}/Dialog/GetOpenedChatsAsJson`, {
+                params: {}
+            })
+            .toPromise()
+            .then(res => {
+                console.log(res);
+            }, console.log);
+    }
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
+    public barChartOptions: ChartOptions = {
+        responsive: true,
+        // We use these empty structures as placeholders for dynamic theming.
+        scales: { xAxes: [{}], yAxes: [{}] },
+        plugins: {
+            datalabels: {
+                anchor: "end",
+                align: "end"
+            }
+        }
+    };
 
+    public barChartLabels: Label[] = [
+        "2006",
+        "2007",
+        "2008",
+        "2009",
+        "2010",
+        "2011",
+        "2012"
+    ];
+    public barChartType: ChartType = "bar";
+    public barChartLegend = true;
+    public barChartPlugins = [];
+
+    public barChartData: ChartDataSets[] = [
+        { data: [65, 59, 80, 81, 56, 55, 40], label: "Series A" },
+        { data: [28, 48, 40, 19, 86, 27, 90], label: "Series B" }
+    ];
 }
